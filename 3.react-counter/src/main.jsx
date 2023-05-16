a828c9198a33c154cf3b516e7f05ce55d1c1d758
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-05-16 21:16:47 +0330

    add 3

--- 3.react-counter/src/main.jsx
+++ 3.react-counter/src/main.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import store from './redux/store.js'
-import { counterDecrementAction, counterIncrementAction, counterSetZeroAction } from './redux/actioncreators.js'
+import { counterDecrementAction , counterIncrementAction , counterSetZeroAction } from './redux/actioncreators.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const render = () =>
-  root.render(
-    <React.StrictMode>
-      <App
-        counter={store.getState()}
-        onIncrement={() => store.dispatch(counterIncrementAction())}
-        onDecrement={() => store.dispatch(counterDecrementAction())}
-        onSetzero={() => store.dispatch(counterSetZeroAction())}
-      />
-    </React.StrictMode>,
-  )
+const render = () => root.render(
+  <React.StrictMode>
+    <App 
+      counter = {store.getState()}
+      onIncrement = {() => store.dispatch(counterIncrementAction())}
+      onDecrement = {() => store.dispatch(counterDecrementAction())}
+      onSetzero = {() => store.dispatch(counterSetZeroAction())}
+    />
+  </React.StrictMode>,
+)
 
 render();
 
